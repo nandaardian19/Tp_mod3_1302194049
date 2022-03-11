0d72550f73fbc30db03ebabb1d374f4bec505509
nandaardian19 <93126096+nandaardian19@users.noreply.github.com>
2022-03-11 08:28:33 +0700

    Add files via upload

---
+++ tpmod3_no1_1302194049/tpmod3_no1_1302194049/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace tpmod3_no1_1302194049
+
+{
+
+{
+	public class KodePos
+{
+	public string getKodePos(string a)
+	{
+		if (a == "Batununggal")
+		{
+			return "40266";
+		}
+		else if (a == "Kujangsari")
+		{
+			return "40287";
+		}
+		else if (a == "Mengger")
+		{
+			return "40267";
+		}
+		else if (a == "Wates")
+		{
+			return "40256";
+		}
+		else if (a == "Cijaura")
+		{
+			return "40287";
+		}
+		else if (a == "Jatisari")
+		{
+			return "40286";
+		}
+		else if (a == "Margasari")
+		{
+			return "40286";
+		}
+		else if (a == "Sekejati")
+		{
+			return "40286";
+		}
+		else if (a == "Kebonwaru")
+		{
+			return "40272";
+		}
+		else if (a == "Maleer")
+		{
+			return "40274";
+		}
+		else if (a == "Samoja")
+		{
+			return "40273";
+		}
+		return "kode pos tidak ditemukan";
+
+	}
+}
+}
---
+++ tpmod3no3_1302194049/tpmod3no3_1302194049/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod3no3_1302194049
+{
+    internal class Program
+    {
+        public class Doormachine
+        {
+            public void pintu (String command)
+            {
+                command = command.ToLowerCase();
+                switch (command)
+                {
+                    case "bukapintu":
+                        this.state = "Terbuka";
+                        break;
+                    case "kuncipintu":
+                        this.state = "Terkunci";
+                        break
+                            
+
+
+                }
+
+            }
+            public String getState ()
+            {
+                return this.state
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
